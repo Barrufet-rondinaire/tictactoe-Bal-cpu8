ddc50ae1b4943433db2a7792ea026e4ee98883a3
bal <erbalvisan@gmail.com>
2025-03-24 15:47:05 +0100

    TicTacToe

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,9 +1,31 @@
-﻿namespace TicTacToe;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 
-class Program
+namespace TicTacToeME
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static async Task Main(string[] args)
+        {
+            var url = "http://localhost:8080/";
+            Uri uri = new(url);
+
+            Console.WriteLine(uri.Host);
+            Console.WriteLine(uri.Port);
+
+            using HttpClient client = new() { BaseAddress = uri };
+
+            // Obtener la llista de jugadors
+            var jugadors = await client.GetFromJsonAsync<List<string>>("jugadors");
+            
+            Console.WriteLine("\nJugadors disponibles:");
+            foreach (var jugador in jugadors)
+            {
+                Console.WriteLine("- " + jugador);
+            }
+        }
     }
 }
